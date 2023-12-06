bafef43c56a37125059f27f26c4b91788cc983e8
Work <marvin.apel@yahoo.com>
2023-12-06 11:51:03 +0100

    Release 1.0

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import find_packages, setup
 
 setup(
     name='doselib',
-    version='0.1',
-    author='Marvin Apel',
+    version='1.0',
+    author='Marvin Apel aka Apelova',
     url = "https://github.com/Apelova/EGS_DOSE_TOOLS",
     description='Library to extract Data or freatures from EGSnrc Monte-Carlo Simulation results',
     maintainer_email = "Marvin.Apel.99@gmail.com",
