834cb64f7f9f3d79c94f2efa663b2008e97ec5f9
C0RDYCEPZ <marvin.apel@yahoo.com>
2024-01-17 10:53:10 +0100

    added monospace in comparison

--- example_compare_dose.py
+++ example_compare_dose.py
@@ -57,82 +57,107 @@ def get_metric_string(DOSE_OBJ, AXIS, exclude={}):
                   "X": ["D0_x", "penumbra_xl", "penumbra_xr", "H_x", "S_x", "dCAX_x"],
                   "Y": ["D0_y", "penumbra_yl", "penumbra_yr", "H_y", "S_y", "dCAX_y"]}
     UNITS = {
-        "D0": "%",
+        "D0": "% ",
         "Rmax": "cm",
-        "D10": "%",
-        "D20": "%",
-        "Q": " ",
-        "D0_x": "%",
+        "D10": "% ",
+        "D20": "% ",
+        "Q": "  ",
+        "D0_x": "% ",
         "penumbra_xl": "cm",
         "penumbra_xr": "cm",
-        "H_x": " ",
-        "S_x": " ",
+        "H_x": "  ",
+        "S_x": "  ",
         "dCAX_x": "cm",
-        "D0_y": "%",
+        "D0_y": "% ",
         "penumbra_yl": "cm",
         "penumbra_yr": "cm",
-        "H_y": " ",
-        "S_y": " ",
+        "H_y": "  ",
+        "S_y": "  ",
         "dCAX_y": "cm" } 
+    def add_digits_to_string(substring):
+        output = ""
+        if "-0." in substring:
+            output += " "
+        elif len(substring)<6:
+            output += " "
+            if "0." in substring:
+                output += " "                            
+        output += substring
+        return output
 
     if AXIS in exclude:
         ATTRIBUTES[AXIS] = [attr for attr in ATTRIBUTES[AXIS] if attr not in exclude[AXIS]] 
 
+    #--- set amount of space such that "attr:" is equidistant (: at same position)
+    dL = max([len(rename_attr(attr)) for attr in ATTRIBUTES[AXIS]])
     # --- gather requested  information in string and output
     metric_string = ""
     for attr in ATTRIBUTES[AXIS]:
         if AXIS.upper() == "Z":
-            metric_string += f"{rename_attr(attr):4}:{getattr(DOSE_OBJ, attr): 7.3f}{UNITS[attr]}\n"
-            print(metric_string )
-            #metric_string += f"{rename_attr(attr)}:{getattr(DOSE_OBJ, attr): .3f}{UNITS[attr]}\n"
+            metric_string += f"{rename_attr(attr):<{dL}}: "
+            metric_string += add_digits_to_string(f"{getattr(DOSE_OBJ, attr):.2f}") 
+            metric_string += f"{UNITS[attr]}\n"
         else:
-            metric_string += f"{rename_attr(attr):7}:{round(getattr(DOSE_OBJ, attr),3): 3}{UNITS[attr]}\n"
-            #metric_string += f"{rename_attr(attr)}:{getattr(DOSE_OBJ, attr): >.3f}{UNITS[attr]}\n"
+            metric_string += f"{rename_attr(attr):<{dL}}: "
+            metric_string += add_digits_to_string(f"{getattr(DOSE_OBJ, attr):.2f}")
+            metric_string += f"{UNITS[attr]}\n"
     # --- remove last "\n"
     return metric_string[:-1]
 
-
-def set_metrics(dose_objs, plot_axs, difference=True, exclude= {}):
+def set_metrics(dose_objs, plot_axs, axes=["Z", "X", "Y"], difference=True, exclude= {}):
     dh_x = 6-len(exclude["X"]) if "X" in exclude else 6
     dh_y = 6-len(exclude["Y"]) if "Y" in exclude else 6
+    # Das hier umschreiben !!! so die reihenfolge mit axes stimmt !
+
     for  i, dose in enumerate(dose_objs[:2]):#compare maximum two dose objs !
-        if difference:
-            # Z-Axis
-            props = dict(boxstyle='square', facecolor=colors[i], edgecolor="black", alpha=0.25, lw=3)
-            metric_string = get_metric_string(dose, AXIS="Z", exclude=exclude)
-            axs[0,0].text(0.7+i*0.27, 0.94, metric_string, transform=axs[0,0].transAxes, fontsize=20, bbox=props, ha="right", va="top")
-            # X-Axis
-            props = dict(boxstyle='square', facecolor=colors[i], edgecolor="black", alpha=0.25, lw=3)
-            metric_string = get_metric_string(dose, AXIS="X", exclude=exclude)
-            axs[0,1].text(0.98, 0.94-i*0.05*dh_x, metric_string, transform=axs[0,1].transAxes, fontsize=20, bbox=props, ha="right", va="top")
-            # Y-Axis
-            props = dict(boxstyle='square', facecolor=colors[i], edgecolor="black", alpha=0.25, lw=3)
-            metric_string = get_metric_string(dose, AXIS="Y", exclude=exclude)
-            axs[0,2].text(0.98, 0.94-i*0.05*dh_y, metric_string, transform=axs[0,2].transAxes, fontsize=20, bbox=props, ha="right", va="top")
-        else:
-            # Z-Axis
-            props = dict(boxstyle='square', facecolor=colors[i], edgecolor="black", alpha=0.25, lw=3)
-            metric_string = get_metric_string(dose, AXIS="Z", exclude=exclude)
-            axs[0].text(0.45+i*0.28, 0.77, metric_string, transform=axs[0,0].transAxes, fontsize=20, bbox=props, ha="right", va="top")
-            # X-Axis
-            props = dict(boxstyle='square', facecolor=colors[i], edgecolor="black", alpha=0.25, lw=3)
-            metric_string = get_metric_string(dose, AXIS="X", exclude=exclude)
-            axs[1].text(0.98, 0.95-i*0.29, metric_string, transform=axs[0,1].transAxes, fontsize=20, bbox=props, ha="right", va="top")
-            # Y-Axis
-            props = dict(boxstyle='square', facecolor=colors[i], edgecolor="black", alpha=0.25, lw=3)
-            metric_string = get_metric_string(dose, AXIS="Y", exclude=exclude)
-            axs[2].text(0.98, 0.95-i*0.29, metric_string, transform=axs[0,2].transAxes, fontsize=20, bbox=props, ha="right", va="top")
+        for j, ax in enumerate(axes):
+            if difference:
+                # Match statements require python 3,8 -> not available in the office therefore use this shananigans
+                if ax == "Z":
+                    props = dict(boxstyle='square', facecolor=colors[i], edgecolor="black", alpha=0.25, lw=3)
+                    metric_string = get_metric_string(dose, AXIS="Z", exclude=exclude)
+                    axs[0,j].text(0.69+i*0.29, 0.94, metric_string, transform=axs[0,j].transAxes, fontsize=19, bbox=props, ha="right", va="top", font="monospace")
+                elif ax =="X":
+                    props = dict(boxstyle='square', facecolor=colors[i], edgecolor="black", alpha=0.25, lw=3)
+                    metric_string = get_metric_string(dose, AXIS="X", exclude=exclude)
+                    axs[0,j].text(0.98, 0.94-i*0.05*dh_x, metric_string, transform=axs[0,j].transAxes, fontsize=19, bbox=props, ha="right", va="top", font="monospace")
+                elif ax =="Y":
+                    props = dict(boxstyle='square', facecolor=colors[i], edgecolor="black", alpha=0.25, lw=3)
+                    metric_string = get_metric_string(dose, AXIS="Y", exclude=exclude)
+                    axs[0,j].text(0.98, 0.94-i*0.05*dh_y, metric_string, transform=axs[0,j].transAxes, fontsize=19, bbox=props, ha="right", va="top", font="monospace")
+            else:
+                if ax =="Z":
+                    props = dict(boxstyle='square', facecolor=colors[i], edgecolor="black", alpha=0.25, lw=3)
+                    metric_string = get_metric_string(dose, AXIS="Z", exclude=exclude)
+                    axs[j].text(0.76+i*0.22, 0.94, metric_string, transform=axs[j].transAxes, fontsize=15, bbox=props, ha="right", va="top", font="monospace")
+                elif ax =="X":
+                    props = dict(boxstyle='square', facecolor=colors[i], edgecolor="black", alpha=0.25, lw=3)
+                    metric_string = get_metric_string(dose, AXIS="X", exclude=exclude)
+                    axs[j].text(0.98, 0.94-i*0.06*(dh_x+1), metric_string, transform=axs[j].transAxes, fontsize=15, bbox=props, ha="right", va="top", font="monospace")
+                elif ax =="Y":
+                    props = dict(boxstyle='square', facecolor=colors[i], edgecolor="black", alpha=0.25, lw=3)
+                    metric_string = get_metric_string(dose, AXIS="Y", exclude=exclude)
+                    axs[j].text(0.98, 0.94-i*0.06*(dh_y+1), metric_string, transform=axs[j].transAxes, fontsize=15, bbox=props, ha="right", va="top", font="monospace")
+     
+# TODO in the office
+# Incorporate in main
+# Test cells below
+# Test box position when exluding !
 
-
+exludio = {"Z":["D20", "D10"], "X":[], "Y":[]}        
+axerinios = ["Z", "X", "Y"]
+differencio = False
 colors_ = colors=["black", "blue"]
-fig, axs = compare_dose([dose_exp, dose_chamber], labels=["Experimental","EGS_CHAMBER"], axes=[
-                        "Z", "X", "Y"], difference=True, interpol="quadratic", diff_dx=0.1, colors=colors_)
 
-set_metrics([dose_exp, dose_chamber], axs, difference=True, exclude = {"X": ["penumbra_xl", "penumbra_xr"], "Y": ["penumbra_yl", "penumbra_yr"], "Z": []})
+fig, axs = compare_dose([dose_exp, dose_chamber], labels=["Experimental","EGS_CHAMBER"], axes=axerinios, difference=differencio, interpol="quadratic", diff_dx=0.1, colors=colors_)
+set_metrics([dose_exp, dose_chamber], axs, axes=axerinios, difference=differencio, exclude = exludio)
+
+
+
 
 
 # %%
-#--- axis setting
+#--- axis settinggrey
 axs[0, 0].legend(loc="lower left", fontsize=30)
 axs[0, 0].set_xlim(0, 30)
 axs[0, 1].set_xlim(-15, 15)
@@ -169,11 +194,20 @@ for  i, dose in enumerate([dose_exp, dose_chamber]):
     # X-Axis
     props = dict(boxstyle='square', facecolor=colors[i], edgecolor="black", alpha=0.25, lw=3)
     metric_string = get_metric_string(dose, AXIS="X")
-    axs[0,0].text(0.98, 0.95-i*0.3, metric_string, transform=axs[0,0].transAxes, fontsize=20, bbox=props, ha="right", va="top")
+    axs[0,0].text(0.98, 0.95-i*0.3, metric_string, transform=axs[0,0].transAxes, fontsize=19, bbox=props, ha="right", va="top")
     # Y-Axis
     props = dict(boxstyle='square', facecolor=colors[1], edgecolor="black", alpha=1, lw=0.25)
     metric_string = get_metric_string(dose, AXIS="Y", )
-    axs[0,1].text(0.98, 0.95-i*0.3, metric_string, transform=axs[0,1].transAxes, fontsize=20, bbox=props, ha="right", va="top")
+    axs[0,1].text(0.98, 0.95-i*0.3, metric_string, transform=axs[0,1].transAxes, fontsize=19, bbox=props, ha="right", va="top")
+
+
+
+
+
+
+
+
+
 
 
 
